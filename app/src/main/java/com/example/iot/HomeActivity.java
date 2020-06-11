86f8b356c2b43e0e2353db205209fe9463668b1d
pnt <phat.nguyentien@hcmut.edu.vn>
2020-06-11 10:43:17 +0700

    Update display username on Homepage

--- app/src/main/java/com/example/iot/HomeActivity.java
+++ app/src/main/java/com/example/iot/HomeActivity.java
@@ -1,14 +1,39 @@
 package com.example.iot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.iot.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private TextView username;
+    private String admindata = "admin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        username = (TextView) findViewById(R.id.tv_admin);
+        FirebaseDatabase dataUser = FirebaseDatabase.getInstance();
+        DatabaseReference RootRef = dataUser.getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Users adminData = dataSnapshot.child("Admin").child(admindata).getValue(Users.class);
+                username.setText(adminData.getUsername().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
