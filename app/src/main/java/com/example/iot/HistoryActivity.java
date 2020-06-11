055d18628b4f61433637835e39a704725133276a
pnt <phat.nguyentien@hcmut.edu.vn>
2020-06-12 03:36:35 +0700

    Try history table

--- app/src/main/java/com/example/iot/HistoryActivity.java
+++ app/src/main/java/com/example/iot/HistoryActivity.java
@@ -1,5 +1,6 @@
 package com.example.iot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -13,7 +14,16 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.example.iot.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HistoryActivity extends AppCompatActivity {
+    private TextView username;
+    private String admindata = "admin";
     private ImageView btn_search;
     private EditText timeData;
     @Override
@@ -22,6 +32,22 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
         btn_search = (ImageView) findViewById(R.id.i_btn_search);
         timeData = (EditText) findViewById(R.id.et_history);
+
+        username = (TextView) findViewById(R.id.tv_admin_history);
+        FirebaseDatabase dataUser = FirebaseDatabase.getInstance();
+        DatabaseReference RootRef = dataUser.getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Users adminData = dataSnapshot.child("Admin").child(admindata).getValue(Users.class);
+                username.setText(adminData.getUsername().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
