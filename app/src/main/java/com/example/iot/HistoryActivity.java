c2fba33c24cf7d1aa0975a9b2932fb8e99fe8f43
pnt <phat.nguyentien@hcmut.edu.vn>
2020-06-11 17:01:16 +0700

    Edit HistoryPage

--- app/src/main/java/com/example/iot/HistoryActivity.java
+++ app/src/main/java/com/example/iot/HistoryActivity.java
@@ -5,17 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class HistoryActivity extends AppCompatActivity {
+    private ImageView btn_search;
+    private EditText timeData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        init();
+        btn_search = (ImageView) findViewById(R.id.i_btn_search);
+        timeData = (EditText) findViewById(R.id.et_history);
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+
     }
 
     @SuppressLint("SetTextI18n")
@@ -38,6 +52,26 @@ public class HistoryActivity extends AppCompatActivity {
         tv_time.setPadding(0,0,0,0);
         tb_row.addView(tv_time);
         table.addView(tb_row);
+        for (int i = 0 ; i < 10 ; i++)
+        {
+            TableRow tbrow = new TableRow(this);
+            TextView tv_area_ind = new TextView(this);
+            tv_area_ind.setText("AREA");
+            tv_area_ind.setTextColor(Color.BLACK);
+            tv_area_ind.setPadding(50,0,380,0);
+            tbrow.addView(tv_area_ind);
+            TextView tv_value_ind = new TextView(this);
+            tv_value_ind.setText("VALUE");
+            tv_value_ind.setTextColor(Color.BLACK);
+            tv_value_ind.setPadding(0,0,220,0);
+            tbrow.addView(tv_value_ind);
+            TextView tv_time_ind = new TextView(this);
+            tv_time_ind.setText("TIME");
+            tv_time_ind.setTextColor(Color.BLACK);
+            tv_time_ind.setPadding(0,0,0,0);
+            tbrow.addView(tv_time_ind);
+            table.addView(tbrow);
+        }
 
     }
 }
