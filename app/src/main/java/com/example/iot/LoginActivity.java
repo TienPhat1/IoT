c2ef7c4f0480158b4962258719141529405f3daa
PhatNg <nguyentienphat3614@gmail.com>
2020-06-11 10:20:54 +0700

    Creating login feature

---
+++ app/src/main/java/com/example/iot/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email,password;
+    private TextView forget_password;
+    private Button btn_login;
+    private ProgressBar loadingBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.et_email);
+        password = (EditText) findViewById(R.id.et_password);
+        forget_password = (TextView) findViewById(R.id.tv_forget_pwd);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        loadingBar = (ProgressBar) findViewById(R.id.probar_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validatedAccount();
+            }
+        });
+    }
+
+    private void validatedAccount() {
+        String user = email.getText().toString();
+        String pwd = password.getText().toString();
+        if(TextUtils.isEmpty(user))
+        {
+            Toast.makeText(this,"Please write email....",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this,"Please write password....",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setVisibility(View.VISIBLE);
+        }
+    }
+}
