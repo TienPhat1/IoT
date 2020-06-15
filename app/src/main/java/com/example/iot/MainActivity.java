9b4aa24db615b332bc1d93d06cf391014abc510d
pnt <phat.nguyentien@hcmut.edu.vn>
2020-06-15 09:56:42 +0700

    Add MQTTHelper

--- app/src/main/java/com/example/iot/MainActivity.java
+++ app/src/main/java/com/example/iot/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this,ControllLightActivity.class);
