f9a9a078490df9db26f375dff48d9c4b3340f820
pnt <phat.nguyentien@hcmut.edu.vn>
2020-06-16 21:10:22 +0700

    Fix error

--- app/src/main/java/com/example/iot/MQTTHelper.java
+++ app/src/main/java/com/example/iot/MQTTHelper.java
@@ -1,5 +1,4 @@
 package com.example.iot;
-<<<<<<< HEAD
 import android.content.Context;
 import android.util.Log;
 
@@ -111,8 +110,4 @@ public class MQTTHelper {
         }
 
     }
-=======
-
-public class MQTTHelper {
->>>>>>> 9b4aa24db615b332bc1d93d06cf391014abc510d
 }
